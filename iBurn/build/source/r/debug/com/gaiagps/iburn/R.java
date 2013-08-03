16d7b3e018ee381f277478cea9f09cae6f62cc87
David Brodsky <davidpbrodsky@gmail.com>
2013-08-03 15:32:15 -0700

    base map activity

--- iBurn/build/source/r/debug/com/gaiagps/iburn/R.java
+++ iBurn/build/source/r/debug/com/gaiagps/iburn/R.java
@@ -179,8 +179,9 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020018;
     }
     public static final class id {
-        public static final int action_settings=0x7f050005;
+        public static final int action_settings=0x7f050006;
         public static final int hybrid=0x7f050004;
+        public static final int map=0x7f050005;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
--- iBurn/src/main/java/com/gaiagps/iburn/MainActivity.java
+++ iBurn/src/main/java/com/gaiagps/iburn/MainActivity.java
@@ -2,9 +2,20 @@ package com.gaiagps.iburn;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import com.cocoahero.android.gmaps.addons.mapbox.MapBoxOfflineTileProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
 
-public class MainActivity extends Activity {
+import java.io.File;
+
+public class MainActivity extends FragmentActivity {
+
+    MapBoxOfflineTileProvider tileProvider;
+    TileOverlay overlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,22 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    private void addMBTileOverlay(String pathToMBTileDatabase){
+        GoogleMap map = ((SupportMapFragment) this.getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        TileOverlayOptions opts = new TileOverlayOptions();
+
+        File MBTFile = new File(pathToMBTileDatabase);
+        tileProvider = new MapBoxOfflineTileProvider(MBTFile);
+        opts.tileProvider(tileProvider);
+        overlay = map.addTileOverlay(opts);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        tileProvider.close();
     }
     
 }
