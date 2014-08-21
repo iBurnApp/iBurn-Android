08b1f5e895bc9b0f29f6c6fe3458f9a2ca70053b
David Brodsky <davidpbrodsky@gmail.com>
2014-08-21 01:34:28 -0700

    Add database migration for version 2 to 3

--- iBurn/src/main/java/com/gaiagps/iburn/database/DBWrapper.java
+++ iBurn/src/main/java/com/gaiagps/iburn/database/DBWrapper.java
@@ -1,10 +1,14 @@
 package com.gaiagps.iburn.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.gaiagps.iburn.PlayaClient;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.ArrayList;
 
 
 /**
@@ -38,10 +42,54 @@ public class DBWrapper extends SQLiteAssetHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db,
        int oldVersion, int newVersion) {
-        // Drop old table and re-create
-    	db.execSQL("DROP TABLE IF EXISTS " + PlayaDatabase.CAMPS);
-    	db.execSQL("DROP TABLE IF EXISTS " + PlayaDatabase.EVENTS);
-    	db.execSQL("DROP TABLE IF EXISTS " + PlayaDatabase.ART);
-		onCreate(db);
+
+        if (oldVersion == 2 && newVersion == 3) {
+            // Add UserPoiTable
+            // Restore all favorites from camps, art, events
+            ArrayList<Integer> favoriteArtPlayaIds          = new ArrayList<>();
+            recordAllFavoriteIds(db, PlayaDatabase.ART,     favoriteArtPlayaIds);
+
+            ArrayList<Integer> favoriteCampPlayaIds         = new ArrayList<>();
+            recordAllFavoriteIds(db, PlayaDatabase.CAMPS,   favoriteCampPlayaIds);
+
+            ArrayList<Integer> favoriteEventPlayaIds        = new ArrayList<>();
+            recordAllFavoriteIds(db, PlayaDatabase.EVENTS,  favoriteEventPlayaIds);
+
+            recreateAllTables(db);
+
+            db.beginTransaction();
+            commitAllFavorites(db, PlayaDatabase.ART,       favoriteArtPlayaIds);
+            commitAllFavorites(db, PlayaDatabase.CAMPS,     favoriteCampPlayaIds);
+            commitAllFavorites(db, PlayaDatabase.EVENTS,    favoriteEventPlayaIds);
+            db.endTransaction();
+        } else {
+            recreateAllTables(db);
+        }
+
+    }
+
+    private void recreateAllTables(SQLiteDatabase db) {
+        db.execSQL("DROP TABLE IF EXISTS " + PlayaDatabase.CAMPS);
+        db.execSQL("DROP TABLE IF EXISTS " + PlayaDatabase.EVENTS);
+        db.execSQL("DROP TABLE IF EXISTS " + PlayaDatabase.ART);
+        onCreate(db);
+    }
+
+    private void recordAllFavoriteIds(SQLiteDatabase db, String table, ArrayList<Integer> favoritePlayaIds) {
+        Cursor favoriteArt = db.query(table, new String[] { PlayaItemTable.playaId }, PlayaItemTable.favorite + " = ?", new String[] {"1"}, null, null, null);
+        if (favoriteArt != null) {
+            while (favoriteArt.moveToNext()) {
+                favoritePlayaIds.add(favoriteArt.getInt(favoriteArt.getColumnIndex(PlayaItemTable.playaId)));
+            }
+            favoriteArt.close();
+        }
+    }
+
+    private void commitAllFavorites(SQLiteDatabase db, String table, ArrayList<Integer> favoritePlayaIds) {
+        ContentValues values = new ContentValues();
+        values.put(PlayaItemTable.favorite, 1);
+        for (Integer favoritePlayaId : favoritePlayaIds) {
+            db.update(table, values, PlayaItemTable.playaId + " = ?", new String[] { String.valueOf(favoritePlayaId)});
+        }
     }
 }
