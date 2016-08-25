345b8df09620b1fede7ad4649a068e7dc9f61982
David Brodsky <davidpbrodsky@gmail.com>
2016-08-24 19:52:35 -0700

    update mock date

--- iBurn/src/main/java/com/gaiagps/iburn/CurrentDateProvider.java
+++ iBurn/src/main/java/com/gaiagps/iburn/CurrentDateProvider.java
@@ -12,7 +12,7 @@ public class CurrentDateProvider {
     /**
      * Date to use as "now" for Debug builds
      */
-    public static Date MOCK_NOW_DATE = new GregorianCalendar(2015, Calendar.AUGUST, 31, 15, 43).getTime();
+    public static Date MOCK_NOW_DATE = new GregorianCalendar(2016, Calendar.SEPTEMBER, 4, 15, 43).getTime();
 
     public static Date getCurrentDate() {
         return BuildConfig.MOCK ? MOCK_NOW_DATE : new Date();
