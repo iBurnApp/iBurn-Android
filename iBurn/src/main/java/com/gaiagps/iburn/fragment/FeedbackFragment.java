857a14a4354819838ead028957a4e8d367f592c6
David Brodsky <davidpbrodsky@gmail.com>
2016-08-29 23:13:08 -0700

    Add app info to feedback email template

--- iBurn/src/main/java/com/gaiagps/iburn/fragment/FeedbackFragment.java
+++ iBurn/src/main/java/com/gaiagps/iburn/fragment/FeedbackFragment.java
@@ -9,7 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.gaiagps.iburn.BuildConfig;
 import com.gaiagps.iburn.R;
+
+import java.util.Locale;
 
 public class FeedbackFragment extends Fragment {
 
@@ -36,11 +39,15 @@ public class FeedbackFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_feedback, container, false);
         feedbackView = (EditText) root.findViewById(R.id.feedback);
 
+        String appInfo = String.format(Locale.US, "(Version: %s %s %s %d %s)\n\n",
+                BuildConfig.APPLICATION_ID, BuildConfig.BUILD_TYPE, BuildConfig.FLAVOR,
+                BuildConfig.VERSION_CODE, BuildConfig.VERSION_NAME);
+
         root.findViewById(R.id.feedbackButton).setOnClickListener(v -> {
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                     "mailto","support+42fb65d77b094805a8b42ac4a6e6a571@feedback.hockeyapp.net", null));
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "iBurn-Android feedback");
-            emailIntent.putExtra(Intent.EXTRA_TEXT, feedbackView.getText());
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "iBurn-Android Feedback");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, appInfo + feedbackView.getText());
             startActivity(Intent.createChooser(emailIntent, "Email Feedback"));
             feedbackView.setText("");
         });
