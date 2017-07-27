7ff392f3fa8897b41b0e7e61871230366ffb8f2b
David Brodsky <dbro@dbro.pro>
2017-07-27 14:54:25 -0700

    BtManager: Enable BT if powered off necessary

--- galactic-jungle/src/main/java/com/gj/animalauto/bt/BtManager.kt
+++ galactic-jungle/src/main/java/com/gj/animalauto/bt/BtManager.kt
@@ -30,6 +30,8 @@ public class BtManager(val context: Context) {
             return
         }
 
+        if (!btAdapter.isEnabled) btAdapter.enable()
+
         discoveryCallback = callback
 
         btAdapter.startDiscovery()
