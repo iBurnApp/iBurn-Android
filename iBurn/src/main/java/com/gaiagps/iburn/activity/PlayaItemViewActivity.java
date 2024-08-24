d614e880310e09c52403e3075b01489225d8f0b1
David Brodsky <davidpbrodsky@gmail.com>
2024-08-24 14:06:48 -0700

    2024.3 Version 56 - Audio Tour  (#56)
    * data update
    
    * fix glitch where art image may appear prematurely before map animation completes
    
    * 2024.3 code 55
    
    * actually fix art image / map intro
    
    * update audio playback service for Android API 34
    
    * version 56

--- iBurn/src/main/java/com/gaiagps/iburn/activity/PlayaItemViewActivity.java
+++ iBurn/src/main/java/com/gaiagps/iburn/activity/PlayaItemViewActivity.java
@@ -511,8 +511,8 @@ public class PlayaItemViewActivity extends AppCompatActivity implements AdapterL
                     FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
             artImageView.setLayoutParams(params);
             artImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            artImageView.setAlpha(.001f); // Hack - Can't seem to properly add view if it's visibility is not VISIBLE or alpha 0. This lets us know that the view isn't technically visible
-            binding.mapContainer.addView(artImageView);
+            artImageView.setAlpha(.99f); // Hack - Can't seem to properly add view if it's visibility is not VISIBLE or alpha 0. This lets us know that the view isn't technically visible
+            binding.mapContainer.addView(artImageView, 0);
 
             loadArtImage(art, artImageView, new com.gaiagps.iburn.Callback() {
 
