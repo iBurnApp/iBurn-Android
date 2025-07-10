44048c28fad14065d9b5b653f6c2778459280c5b
David Brodsky <davidpbrodsky@gmail.com>
2025-07-09 17:38:10 -0700

    Re-use fragments when navigating tabs

--- iBurn/src/main/java/com/gaiagps/iburn/activity/MainActivity.java
+++ iBurn/src/main/java/com/gaiagps/iburn/activity/MainActivity.java
@@ -73,6 +73,15 @@ public class MainActivity extends AppCompatActivity implements SearchQueryProvid
     private PrefsHelper prefs;
     private String searchQuery;
     private BottomNavigationView bottomBar;
+    
+    // Fragment instances for reuse
+    private Fragment mapFragment;
+    private Fragment mapPlaceholderFragment;
+    private Fragment exploreFragment;
+    private Fragment browseFragment;
+    private Fragment favoritesFragment;
+    private Fragment searchFragment;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -186,9 +195,17 @@ public class MainActivity extends AppCompatActivity implements SearchQueryProvid
         int mapTabId = R.id.tab_map;
 
         if (currentTabId == mapTabId) {
-            setCurrentFragment(awaitingPermission ?
-                    new MapPlaceHolderFragment() :
-                    new MapboxMapFragment());
+            if (awaitingPermission) {
+                if (mapPlaceholderFragment == null) {
+                    mapPlaceholderFragment = new MapPlaceHolderFragment();
+                }
+                setCurrentFragment(mapPlaceholderFragment);
+            } else {
+                if (mapFragment == null) {
+                    mapFragment = new MapboxMapFragment();
+                }
+                setCurrentFragment(mapFragment);
+            }
         }
     }
 
@@ -198,18 +215,36 @@ public class MainActivity extends AppCompatActivity implements SearchQueryProvid
             final int selectedId = menuItem.getItemId();
             if (R.id.tab_map == selectedId) {
                 if (awaitingLocationPermission) {
-                    frag = new MapPlaceHolderFragment();
+                    if (mapPlaceholderFragment == null) {
+                        mapPlaceholderFragment = new MapPlaceHolderFragment();
+                    }
+                    frag = mapPlaceholderFragment;
                 } else {
-                    frag = new MapboxMapFragment();
+                    if (mapFragment == null) {
+                        mapFragment = new MapboxMapFragment();
+                    }
+                    frag = mapFragment;
                 }
             } else if (R.id.tab_now == selectedId) {
-                frag = new ExploreListViewFragment();
+                if (exploreFragment == null) {
+                    exploreFragment = new ExploreListViewFragment();
+                }
+                frag = exploreFragment;
             } else if (R.id.tab_browse == selectedId) {
-                frag = new BrowseListViewFragment();
+                if (browseFragment == null) {
+                    browseFragment = new BrowseListViewFragment();
+                }
+                frag = browseFragment;
             } else if (R.id.tab_favorites == selectedId) {
-                frag = new FavoritesListViewFragment();
+                if (favoritesFragment == null) {
+                    favoritesFragment = new FavoritesListViewFragment();
+                }
+                frag = favoritesFragment;
             } else if (R.id.tab_search == selectedId) {
-                frag = new SearchFragment();
+                if (searchFragment == null) {
+                    searchFragment = new SearchFragment();
+                }
+                frag = searchFragment;
             }
 
             if (frag != null) {
@@ -226,10 +261,26 @@ public class MainActivity extends AppCompatActivity implements SearchQueryProvid
     }
 
     private void setCurrentFragment(@NonNull Fragment fragment) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.content, fragment)
-                .commitAllowingStateLoss();
+        if (fragment == currentFragment) {
+            return; // Fragment is already displayed
+        }
+        
+        androidx.fragment.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        
+        // Hide current fragment if it exists
+        if (currentFragment != null) {
+            transaction.hide(currentFragment);
+        }
+        
+        // Show or add the new fragment
+        if (fragment.isAdded()) {
+            transaction.show(fragment);
+        } else {
+            transaction.add(R.id.content, fragment);
+        }
+        
+        transaction.commitAllowingStateLoss();
+        currentFragment = fragment;
     }
 
     @Override
