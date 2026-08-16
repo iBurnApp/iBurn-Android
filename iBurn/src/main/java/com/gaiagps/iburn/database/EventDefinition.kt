ce5f4a4cd3e435e5a8e41b9c9bae013220d898e3
David Brodsky <davidpbrodsky@gmail.com>
2026-08-16 09:27:30 -0700

    fix lint (#93)

--- iBurn/src/main/java/com/gaiagps/iburn/database/EventDefinition.kt
+++ iBurn/src/main/java/com/gaiagps/iburn/database/EventDefinition.kt
@@ -1,9 +1,11 @@
 package com.gaiagps.iburn.database
 
+import android.annotation.SuppressLint
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 
+@SuppressLint("ParcelCreator")
 @Entity(
     tableName = EventDefinition.TABLE_NAME,
     indices = [
